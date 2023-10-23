c2b92007102c80244d4dd8678343d3031b61a028
Emilse Arias <emilsearias@gmail.com>
2023-10-23 10:34:37 -0300

    declaramos e inicializamos una variable con let y const

---
+++ js/01-variables.js
@@ -0,0 +1,23 @@
+/* Esto es un comentario
+de multiples
+lineas 
+*/
+
+// Esto es un comentario de una unica linea
+
+// declarar una variable ( var - let - const)
+
+let anioActual = 2023;
+const url = "https://universe.rollingcodeschool.com/";
+
+// mostrar mensajes
+console.log(anioActual);
+console.log("anioActual");
+console.log("Año actual: " + anioActual + " hola mundo");
+
+// modificar el valor de una variable
+anioActual = 2023 + 1;
+console.log("Año actual: " + anioActual);
+console.log(url);
+
+
