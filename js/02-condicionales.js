b4b07c42e73ddb63866e8e38d75242f3f325e899
Emilse Arias <emilsearias@gmail.com>
2023-10-25 10:14:25 -0300

    agregamos un ejemplo de condicionales

---
+++ js/02-condicionales.js
@@ -0,0 +1,21 @@
+/*
+if(codincion logica){
+    todo el codigo que quiero ejecutar si se cumple la condicion
+}
+*/
+
+// Solicitar al usuario una edad, si el usuario es mayor a 16 años deberia votar.
+//  entre 16 y 18 es optativo y mayores de 70
+// entre 18 y 70 años inclusive es obligatorio
+
+const edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad >= 16 && edad < 18 || edad > 70 ) {
+  alert("Es optativo votar");
+} else {
+  if(edad >= 18 && edad <= 70){
+    alert("Usted debe votar");
+  }else{
+    alert("No podes votar");
+  }
+}
