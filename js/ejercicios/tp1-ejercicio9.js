3ae3c6061fc7b100bf005370601e98cd1fe8e1a1
Emilse Arias <emilsearias@gmail.com>
2023-10-30 10:02:32 -0300

    agregamos el ejercicio 9 del tp 1

---
+++ js/ejercicios/tp1-ejercicio9.js
@@ -0,0 +1,15 @@
+//length -> cantidad de caracteres de un string
+// frase.chartAt(0);
+// hola mundo
+// frase.toLowerCase() pasa el contenido de la frase a minusculas
+const frase = prompt('Ingrese una frase').toLowerCase();
+// frase = frase.toLowerCase();
+console.log(frase.length);
+console.log(frase);
+console.log(frase.charAt(0));
+
+for(let indiceFrase = 0; indiceFrase < frase.length; indiceFrase++){
+    if( frase.charAt(indiceFrase) == 'a' || frase.charAt(indiceFrase) == 'e' || frase.charAt(indiceFrase) == 'i' || frase.charAt(indiceFrase) == 'o' || frase.charAt(indiceFrase) == 'u'){
+        document.write(frase.charAt(indiceFrase));
+    }
+}
