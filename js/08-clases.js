6f4419a01cd17e2a43f5b1a4654aa64d6432cbef
Emilse Arias <emilsearias@gmail.com>
2023-11-13 11:31:29 -0300

    declaramos una propiedad privada

--- js/08-clases.js
+++ js/08-clases.js
@@ -1,12 +1,13 @@
 // averiguar en que version del ES se agrego la sintaxis de clases
 //anioLanzamiento, desarrollador 
 class VideoJuego{
+    #etiquetas; //propiedad privada
     constructor(tituloParam, generoParam, precioParam, etiquetasParam, anioLanzamientoParam, desarrolladorParam){
         //declaramos las propiedades que tendra un videojuego
         this._titulo = tituloParam;
         this._precio = precioParam;
         this.genero = generoParam;
-        this.etiquetas = etiquetasParam;
+        this.#etiquetas = etiquetasParam;
         this.anioLanzamiento = anioLanzamientoParam;
         this.desarrollador = desarrolladorParam;
         //propiedad por defecto
@@ -33,11 +34,19 @@ class VideoJuego{
     mostrarDatos(){
         document.write(`<ul>
         <li>Titulo: ${this.titulo}</li>
-        <li>Precio: $${this.precio}</li>
-        <li>Genero: $${this.genero}</li>
-        <li>Desarrollador: $${this.desarrollador}</li>
+        <li>Precio: $${this.precio}USD</li>
+        <li>Genero: ${this.genero}</li>
+        <li>Desarrollador: ${this.desarrollador}</li>
         </ul>`)
     }
 
 }
 
+//crear o instanciar un objeto
+
+const minecraft = new VideoJuego('Minecraft', 'sandbox', 30, ['cubitos', 'granja', 'mundo abierto','creeper'], 2008, 'Mojang');
+
+minecraft.mostrarDatos();
+
+document.write(`<p>Titulo del juego: ${minecraft.titulo} , precio: $${minecraft.precio} USD </p>`)
+document.write(`<p>Etiquetas: ${minecraft.etiquetas} </p>`)
